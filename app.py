28d098dc45dfde67cfca90b1ba8b8f7295bf0a29
Ksenia_Sheveleva <brantova00@gmail.com>
2025-07-20 22:02:29 +0300

    Добавлены доработки

--- app.py
+++ app.py
@@ -1,17 +1,32 @@
-from flask import Flask, render_template
+import http.server
+import socketserver
 import os
 
-app = Flask(__name__, template_folder='templates', static_folder='../static')
+PORT = 5000
 
-@app.route('/')
-def home():
-    return render_template('contacts.html')
 
-@app.route('/contacts')
-def contacts():
-    return render_template('contacts.html')
+class CustomHandler(http.server.BaseHTTPRequestHandler):
+    def do_GET(self):
+        # Устанавливаем тип контента
+        self.send_response(200)
+        self.send_header('Content-type', 'text/html')
+        self.end_headers()
 
-# Добавьте другие маршруты по аналогии
+        # Читаем содержимое HTML-файла
+        try:
+            with open('templates/contacts.html', 'rb') as file:
+                content = file.read()
+                self.wfile.write(content)
+        except FileNotFoundError:
+            self.send_error(404, "File not found")
 
-if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+
+# Запуск сервера
+with socketserver.TCPServer(("", PORT), CustomHandler) as httpd:
+    print(f"Сервер запущен на порту {PORT}")
+    try:
+        httpd.serve_forever()
+    except KeyboardInterrupt:
+        pass
+    httpd.server_close()
+    print("Сервер остановлен")
